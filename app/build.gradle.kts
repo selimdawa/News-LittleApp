e182833a64462a925314385031652b37e4dcf4a6
selimdawa <selimdawa@gmail.com>
2026-06-09 03:49:00 +0300

    SDK 37 / Android 17

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,15 +9,13 @@ plugins {
 android {
     namespace = "com.littleapp.news"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+       version = release(37)
     }
 
     defaultConfig {
         applicationId = "com.littleapp.news"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
