6b367daefedfab645ec8bf9a27e40bb4f6d6d518
selimdawa <selimdawa@gmail.com>
2026-07-24 19:24:55 +0300

    agp - 9.3.0

--- app/src/main/java/com/littleapp/news/base/IBaseDiffModel.kt
+++ app/src/main/java/com/littleapp/news/base/IBaseDiffModel.kt
@@ -10,7 +10,7 @@ interface IBaseDiffModel<T> {
 class BaseDiffUtilItemCallback<T : IBaseDiffModel<S>, S> : DiffUtil.ItemCallback<T>() {
 
     override fun areItemsTheSame(oldItem: T, newItem: T): Boolean = oldItem.id == newItem.id
-    
+
     @SuppressLint("DiffUtilEquals")
     override fun areContentsTheSame(oldItem: T, newItem: T): Boolean = oldItem == newItem
 }
