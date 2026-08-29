a58ed6c84a0da56dc56f3e17848cf1ef3e9defb9
selimdawa <selimdawa@gmail.com>
2026-08-29 06:53:25 +0300

    Edge to Edge - Add

--- app/src/main/java/com/littleapp/news/ui/activity/MainActivity.kt
+++ app/src/main/java/com/littleapp/news/ui/activity/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.littleapp.news.ui.activity
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.FragmentManager
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -18,9 +21,15 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         applyAppTheme()
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            insets
+        }
 
         binding.toolbar.nameSpace.text = Constants.NEWS
 
--- app/src/main/java/com/littleapp/news/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/littleapp/news/ui/activity/SplashActivity.kt
@@ -2,7 +2,10 @@ package com.littleapp.news.ui.activity
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
 import com.littleapp.news.databinding.ActivitySplashBinding
 import com.littleapp.news.utils.applyAppTheme
@@ -18,9 +21,15 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         applyAppTheme()
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         lifecycleScope.launch {
             delay(SPLASH_DELAY)
